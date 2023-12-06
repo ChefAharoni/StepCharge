e8a6e1dc9a64aa3b73840b75756d21f892d3e92f
ChefAharoni <ChefAharoni@proton.me>
2023-12-06 17:37:18 -0500

    Added limitation for redemption to allow only once.

--- StepCharge/StepRedeemView.swift
+++ StepCharge/StepRedeemView.swift
@@ -13,6 +13,7 @@ struct StepRedeemView: View {
     @State private var showingMonthlySteps = true
     @State private var showingRedemptionAlert = false
     @State private var credits = 0.0
+    @AppStorage("lastRedemptionDateString") private var lastRedemptionDateString: String?
 
     var body: some View {
         VStack {
@@ -35,8 +36,10 @@ struct StepRedeemView: View {
                 let stepsCredits = calculateCredits(steps: steps)
                 redeemCredits(creditsToAdd: stepsCredits)
                 showingRedemptionAlert = true
+                lastRedemptionDateString = ISO8601DateFormatter().string(from: Date()) // Store the date as a string
             }
-            .disabled(showingMonthlySteps ? steps < 50000 : steps < 10000)
+            .disabled(!canRedeemCredits(steps: steps))
+//            .disabled(showingMonthlySteps ? steps < 50000 : steps < 10000)
             .alert(isPresented: $showingRedemptionAlert) {
                 Alert(title: Text("Credits Redeemed"), message: Text("You have redeemed $\(calculateCredits(steps: steps), specifier: "%.2f") credits."), dismissButton: .default(Text("OK")))
             }
@@ -53,5 +56,21 @@ struct StepRedeemView: View {
 
     func redeemCredits(creditsToAdd: Double) {
         credits += creditsToAdd
+    }
+    
+    func canRedeemCredits(steps: Int) -> Bool {
+        let minimumStepsRequired = showingMonthlySteps ? 50000 : 10000
+        let isEligibleForRedemption = steps >= minimumStepsRequired
+
+        guard let lastRedemptionStr = lastRedemptionDateString,
+              let lastRedemption = ISO8601DateFormatter().date(from: lastRedemptionStr) else {
+            return isEligibleForRedemption // No redemption made yet or invalid date string
+        }
+
+        let calendar = Calendar.current
+        let currentMonth = calendar.component(.month, from: Date())
+        let lastRedemptionMonth = calendar.component(.month, from: lastRedemption)
+
+        return isEligibleForRedemption && (currentMonth != lastRedemptionMonth)
     }
 }
