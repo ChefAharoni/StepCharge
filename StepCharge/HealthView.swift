bc9608939a460098f67502adb44d0e369094e2a4
ChefAharoni <ChefAharoni@proton.me>
2023-12-07 13:30:33 -0500

    * Added GPT description for the DALLE prompt, showing a sentence of a place that will be generated at the top.

--- StepCharge/HealthView.swift
+++ StepCharge/HealthView.swift
@@ -5,93 +5,171 @@
 //  Created by Amit Aharoni on 12/6/23.
 //
 
-import HealthKit
-import Combine
-    
-    import SwiftUI
+ import HealthKit
+ import Combine
+ import SwiftUI
 
-    struct HealthView: View {
-        @ObservedObject var healthKitManager: HealthKitManager
-        var displayMode: DisplayMode
-        @State private var imageUrl: URL?
+ struct HealthView: View {
+     @ObservedObject var healthKitManager: HealthKitManager
+     var displayMode: DisplayMode
+     @State private var imageUrl: URL?
+     @State private var generatedLocation: String = ""
 
-        enum DisplayMode {
-            case daily, monthly
-        }
+     enum DisplayMode {
+         case daily, monthly
+     }
 
-        var body: some View {
-            VStack {
-                if displayMode == .daily {
-                    Text("Daily Steps: \(healthKitManager.dailySteps)")
-                    Button("Generate Image") {
-                        generateImageForSteps(healthKitManager.dailySteps)
-                    }
-                } else {
-                    Text("Monthly Steps: \(healthKitManager.monthlySteps)")
-                    Button("Generate Image") {
-                        generateImageForSteps(healthKitManager.monthlySteps)
-                    }
-                }
+     var body: some View {
+         VStack {
+             if displayMode == .daily {
+                 Text("Daily Steps: \(healthKitManager.dailySteps)")
+                 Button("Generate Image") {
+                     generateImageForSteps(healthKitManager.dailySteps)
+                 }
+             } else {
+                 Text("Monthly Steps: \(healthKitManager.monthlySteps)")
+                 Button("Generate Image") {
+                     generateImageForSteps(healthKitManager.monthlySteps)
+                 }
+             }
+             
+             if !generatedLocation.isEmpty {
+                 Text(timeFrameText + generatedLocation)
+                     .padding()
+             }
 
-                if let imageUrl = imageUrl {
-                    AsyncImage(url: imageUrl) { image in
-                        image.resizable()
-                    } placeholder: {
-                        ProgressView()
-                    }
-                    .frame(width: 200, height: 200)
-                }
-            }
-        }
+             if let imageUrl = imageUrl {
+                 AsyncImage(url: imageUrl) { image in
+                     image.resizable()
+                 } placeholder: {
+                     ProgressView()
+                 }
+                 .frame(width: 200, height: 200)
+             }
+         }
+     }
 
-        private func generateImageForSteps(_ steps: Int) {
-            let description = "A scenic view of a location equivalent to a walk of \(steps) steps" // Customize this prompt as needed
-            requestImageFromDALLE(description: description)
-        }
+     private var timeFrameText: String {
+         switch displayMode {
+         case .daily:
+             return "Today, you have walked the amount of steps that are equivalent to "
+         case .monthly:
+             return "This Month, you have walked the amount of steps that are equivalent to "
+         }
+     }
 
-        private func requestImageFromDALLE(description: String) {
-            let url = URL(string: "https://api.openai.com/v1/images/generations")!
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue("Bearer \(Config.openAIKey)", forHTTPHeaderField: "Authorization")
+     private func generateImageForSteps(_ steps: Int) {
+         generatePromptForSteps(steps) { fullDescription in
+             let location = self.extractLocation(from: fullDescription)
+             DispatchQueue.main.async {
+                 self.generatedLocation = location // Store the location
+             }
+             self.requestImageFromDALLE(description: fullDescription) // Use the full description for the image
+         }
+     }
+     
+     // Function to extract a location from a given text
+     private func extractLocation(from text: String) -> String {
+         let sentences = text.components(separatedBy: ". ")
+         let firstSentence = sentences.first?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+         // Extract the first 2-3 words from the sentence as the location
+         let words = firstSentence.components(separatedBy: " ")
+         let location = words.prefix(3).joined(separator: " ")
+         return location
+     }
 
-            let body: [String: Any] = [
-                "prompt": description,
-                "n": 1,
-                "size": "1024x1024"
-            ]
+     // ...existing code for generatePromptForSteps and requestImageFromDALLE...
+     private func generatePromptForSteps(_ steps: Int, completion: @escaping (String) -> Void) {
+         let url = URL(string: "https://api.openai.com/v1/chat/completions")!
+         var request = URLRequest(url: url)
+         request.httpMethod = "POST"
+         request.addValue("Bearer \(Config.openAIKey)", forHTTPHeaderField: "Authorization")
+         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
 
-            request.httpBody = try? JSONSerialization.data(withJSONObject: body)
+         // Updated prompt to ask for a brief location name
+         let prompt = "Provide a short name (2-3 words) of a location that represents a walk of \(steps) steps."
+         let body: [String: Any] = [
+             "model": "gpt-4",
+             "messages": [["role": "system", "content": prompt]]
+         ]
 
-            URLSession.shared.dataTask(with: request) { data, response, error in
-                guard let data = data, error == nil else {
-                    print("Error: \(error?.localizedDescription ?? "Unknown error")")
-                    return
-                }
-                // Parse the response data
-                if let jsonResponse = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-                   let data = jsonResponse["data"] as? [[String: Any]],
-                   let firstImage = data.first,
-                   let imageUrlString = firstImage["url"] as? String,
-                   let imageUrl = URL(string: imageUrlString) {
-                    
-                    DispatchQueue.main.async {
-                        self.imageUrl = imageUrl // Update imageUrl to display the image
-                    }
-                } else {
-                    print("Failed to parse JSON or extract image URL")
-                }
-            }.resume()
-        }
+         request.httpBody = try? JSONSerialization.data(withJSONObject: body)
+
+     URLSession.shared.dataTask(with: request) { data, response, error in
+         if let error = error {
+             print("GPT-4 Request Error: \(error.localizedDescription)")
+             return
+         }
+
+         // Debug: Print the raw response for inspection
+         if let jsonStr = String(data: data!, encoding: .utf8) {
+             print("Raw JSON Response: \(jsonStr)")
+         }
+
+         guard let data = data else {
+             print("GPT-4 No Data Received")
+             return
+         }
+
+         // Adjusted parsing logic
+         if let jsonResponse = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+            let choices = jsonResponse["choices"] as? [[String: Any]],
+            let lastMessage = choices.first?["message"] as? [String: Any],
+            let text = lastMessage["content"] as? String {
+             
+             DispatchQueue.main.async {
+                 completion(text.trimmingCharacters(in: .whitespacesAndNewlines))
+             }
+         } else {
+             print("Failed to parse GPT-4 JSON or extract text")
+         }
+     }.resume()
+ }
+
+     private func requestImageFromDALLE(description: String) {
+         let url = URL(string: "https://api.openai.com/v1/images/generations")!
+         var request = URLRequest(url: url)
+         request.httpMethod = "POST"
+         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+         request.addValue("Bearer \(Config.openAIKey)", forHTTPHeaderField: "Authorization")
+
+         // Ensure the prompt is suitable for DALL-E
+         let dallePrompt = "A scenic view of \(description)"
+         let body: [String: Any] = [
+             "prompt": dallePrompt,
+             "n": 1,
+             "size": "1024x1024"
+         ]
+
+         request.httpBody = try? JSONSerialization.data(withJSONObject: body)
 
 
-    }
+     URLSession.shared.dataTask(with: request) { data, response, error in
+         if let error = error {
+             print("DALL-E Request Error: \(error.localizedDescription)")
+             return
+         }
+         if let httpResponse = response as? HTTPURLResponse {
+             print("DALL-E Response Status Code: \(httpResponse.statusCode)")
+         }
+         guard let data = data else {
+             print("DALL-E No Data Received")
+             return
+         }
+         if let jsonResponse = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+            let data = jsonResponse["data"] as? [[String: Any]],
+            let firstImage = data.first,
+            let imageUrlString = firstImage["url"] as? String,
+            let imageUrl = URL(string: imageUrlString) {
+             
+             DispatchQueue.main.async {
+                 self.imageUrl = imageUrl // Update imageUrl to display the image
+             }
+         } else {
+             print("Failed to parse DALL-E JSON or extract image URL")
+         }
+     }.resume()
+ }
+ }
 
-    
-//    struct HealthView_Previews: PreviewProvider { // Preview Provider: This struct provides a preview of HealthView in Xcode's canvas.
-//        static var previews: some View {
-//            HealthView()
-//        }
-//    }
-//}
+ 
--- StepCharge/StepRedeemView.swift
+++ StepCharge/StepRedeemView.swift
@@ -13,21 +13,15 @@ struct StepRedeemView: View {
     @State private var showingMonthlySteps = true
     @State private var showingRedemptionAlert = false
     @State private var credits = 0.0
-//    @State private var accountBalance: Double = 0.0 // State to store the account balance
-    @State private var currentAccountBalance: Double = 0.0 // State to store the current account balance
-
-//    @AppStorage("lastRedemptionDateString") private var lastRedemptionDateString: String?
-    
     
     private var accountBalance: Double {
         AccountManager.shared.getBalance(forUser: userName)
     }
-    
 
     var body: some View {
         VStack {
             Text("Welcome, \(userName)")
-            Text("Account Balance: $\(accountBalance, specifier: "%.2f")") // Display account balance
+            Text("Account Balance: $\(accountBalance, specifier: "%.2f")")
             Text("Credits: $\(credits, specifier: "%.2f")")
 
             Toggle("Monthly \\ Daily Steps", isOn: $showingMonthlySteps)
@@ -44,17 +38,13 @@ struct StepRedeemView: View {
 
             Button("Redeem Credits") {
                 let stepsCredits = calculateCredits(steps: steps)
-                if currentAccountBalance >= stepsCredits {
+                if accountBalance >= stepsCredits {
                     redeemCredits(creditsToAdd: stepsCredits)
                     AccountManager.shared.updateBalance(forUser: userName, amount: -stepsCredits)
-                    currentAccountBalance -= stepsCredits // Update local state
                     showingRedemptionAlert = true
                 } else {
                     // Handle case where there isn't enough balance
                 }
-            }
-            .onAppear {
-                currentAccountBalance = AccountManager.shared.getBalance(forUser: userName) // Fetch balance on appear
             }
             .alert(isPresented: $showingRedemptionAlert) {
                 Alert(title: Text("Credits Redeemed"), message: Text("You have redeemed $\(calculateCredits(steps: steps), specifier: "%.2f") credits. Remaining balance: $\(accountBalance, specifier: "%.2f")"), dismissButton: .default(Text("OK")))
@@ -73,20 +63,5 @@ struct StepRedeemView: View {
     func redeemCredits(creditsToAdd: Double) {
         credits += creditsToAdd // Add Credits to the existing credits
     }
-    
-//    func canRedeemCredits(steps: Int) -> Bool {
-//        let minimumStepsRequired = showingMonthlySteps ? 50000 : 10000
-//        let isEligibleForRedemption = steps >= minimumStepsRequired
-//
-//        guard let lastRedemptionStr = lastRedemptionDateString,
-//              let lastRedemption = ISO8601DateFormatter().date(from: lastRedemptionStr) else {
-//            return isEligibleForRedemption // No redemption made yet or invalid date string
-//        }
-//
-//        let calendar = Calendar.current
-//        let currentMonth = calendar.component(.month, from: Date())
-//        let lastRedemptionMonth = calendar.component(.month, from: lastRedemption)
-//
-//        return isEligibleForRedemption && (currentMonth != lastRedemptionMonth)
-//    }
 }
+
