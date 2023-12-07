e81a68f19dee7e58efeb682f0fefb36f96aa31ae
jerry3008 <gbemuduj@gmail.com>
2023-12-07 12:19:51 +0100

    Run your Swift application and navigate to the new tab (AI Interaction). Use the Fetch AI Response button to trigger the AI interaction and display the response in the UI

--- StepCharge/StepsCharge.swift
+++ StepCharge/StepsCharge.swift
@@ -16,6 +16,21 @@ struct StepsCharge: App {
     @State private var showStepRedeemView = false
     @State private var selectedTab: Int = 0
 
+struct AIView: View {
+    @State private var aiResponse: String = ""
+
+    var body: some View {
+        VStack {
+            Text("AI Response:")
+            Text(aiResponse)
+                .padding()
+
+            // Button to trigger AI interaction
+            Button("Fetch AI Response") {
+                fetchAIResponse()
+
+
+	
     var body: some Scene {
         WindowGroup {
             TabView(selection: $selectedTab) {
@@ -42,11 +57,34 @@ struct StepsCharge: App {
                         Label("Redeem", systemImage: "dollarsign.circle")
                     }
                     .tag(3)
+		      // Add a view to trigger the AI interaction
+                AIView(healthKitManager: healthKitManager)
+                    .tabItem {
+                        Label("AI Interaction", systemImage: "ellipsis.bubble")
+                    }
+                    .tag(4)
             
             }
             .onAppear {
                 healthKitManager.queryMonthlyStepCount()  // Fetch monthly steps when the TabView appears
+		 // Fetch AI response when the TabView appears
+                fetchAIResponse()
             }
         }
+    }
+    // Method to fetch AI response
+    func fetchAIResponse() {
+        // Use a networking library or URLSession to fetch data from Cloudflare Worker
+        // You can replace the placeholder URL with the actual URL of your Cloudflare Worker
+        guard let url = URL(string: "https://your-cloudflare-worker-url") else { return }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                // Parse the data and update UI as needed
+                // You may need to decode the response depending on the format
+                let decodedResponse = try? JSONDecoder().decode(ResponseType.self, from: data)
+                // Update UI or perform other actions with the decoded response
+            }
+        }.resume()
     }
 }
